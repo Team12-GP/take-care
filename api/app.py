a0653c1cff0758b0be8ea1507a01189fffbe79ba
Team12-GP <mdhr1082@gmail.com>
2025-08-08 22:32:17 +0600

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,12 +1,22 @@
 import os
+import sys
 import importlib
 from flask import Flask, Blueprint
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# Add parent directory to PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Path to the directory containing route files
 router_path = os.path.join(os.path.dirname(__file__), 'routers')
+
+# Ensure routers is a package
+init_file = os.path.join(router_path, '__init__.py')
+if not os.path.exists(init_file):
+    open(init_file, 'a').close()
 
 # Loop through all Python files in the router directory
 for filename in os.listdir(router_path):
@@ -14,7 +24,7 @@ for filename in os.listdir(router_path):
         # Import the module dynamically
         module_name = f"routers.{filename[:-3]}"
         module = importlib.import_module(module_name)
-        
+
         # Find and register the Blueprint in the module
         for attribute_name in dir(module):
             attribute = getattr(module, attribute_name)
@@ -22,6 +32,5 @@ for filename in os.listdir(router_path):
                 app.register_blueprint(attribute)
 
 if __name__ == '__main__':
-    import os
-    
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
